d568d5edb3391fd304aec0de38dca0b24e818bc9
Khaoula <ktagorti@gmail.com>
2025-02-01 21:05:38 +0900

    add initial components and pages for banking application

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
+import Sidebar from "@/components/Sidebar";
+import MobileNavBar from "@/components/MobileNavBar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: "Khaoula", lastName: "Tagorti" };
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex size-4 flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width="30" height="30" alt="menu icon" />
+          <div>
+            <MobileNavBar user={loggedIn} />
+          </div>
+        </div>
+      </div>
       {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import RightSideBar from "@/components/RightSideBar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 function Home() {
-  return <div>Home</div>;
+  const loggedIn = {
+    firstName: "Khaoula",
+    lastName: "Tagorti",
+    email: "ktagorti@gmail.com",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT Transactions
+      </div>
+      <RightSideBar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 150.6 }]}
+      />
+    </section>
+  );
 }
 
 export default Home;
